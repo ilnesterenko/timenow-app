14ff83dc0d88127aca06e215e083739cd4a3ffa2
ilnesterenko <19206104+ilnesterenko@users.noreply.github.com>
2026-05-26 22:30:49 +0000

    deploy: ready for render

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,16 @@
 from flask import Flask
 from datetime import datetime
+from zoneinfo import ZoneInfo # Імпортуємо роботу з часовими поясами
 
 app = Flask(__name__)  
 
-@app.route('/time')
-def time():
-    now = datetime.now()
+@app.route('/api/time', methods=['GET'])
+def get_time():
+    # Беремо поточний час строго за київським часом
+    now = datetime.now(ZoneInfo("Europe/Kyiv"))
     responseBody = now.strftime("%H:%M:%S")
     return responseBody
 
-# uncomment to modify response headers for all requests
-#@app.after_request
-#def add_header(response):
-#    response.headers['Access-Control-Allow-Origin'] = '*'
-#    return response
-
-app.run(debug=True, port=8080, host="0.0.0.0")
+if __name__ == '__main__':
+    # We work on port 8080, listening to all interfaces inside the future container
+    app.run(debug=True, port=8080, host="0.0.0.0")
